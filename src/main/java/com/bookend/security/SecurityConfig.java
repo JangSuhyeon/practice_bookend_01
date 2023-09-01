136b6886f62d778b31fe3188f06aa1e37061b0f8
JangSuhyeon <shjang0043@gmail.com>
2023-09-01 13:36:38 +0900

    Google OAuth2 연동(2)

--- src/main/java/com/bookend/security/SecurityConfig.java
+++ src/main/java/com/bookend/security/SecurityConfig.java
@@ -19,7 +19,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(AbstractHttpConfigurer::disable) // Todo disable 안하면 어떻게 되는지 확인 필요
                 .authorizeHttpRequests(authorizeHttpRequests ->
                         authorizeHttpRequests
                                 .requestMatchers("/css/**", "/fonts/**", "/vendor/**","/images/**", "/js/**", "/login/**").permitAll() // 해당 url은 권한 없이 접근 가능
@@ -27,9 +26,6 @@ public class SecurityConfig {
                 .formLogin(formLogin ->
                         formLogin
                                 .loginPage("/login/page").permitAll()) // 로그인 페이지 경로 지정
-//                .logout(logout ->
-//                        logout
-//                                .logoutSuccessUrl("/")) // 로그아웃 성공 시 이동할 url
                 .oauth2Login(oauth2Login ->
                         oauth2Login
                                 .userInfoEndpoint(userInfo ->   // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
